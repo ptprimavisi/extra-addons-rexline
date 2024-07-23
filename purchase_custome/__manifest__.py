4760ee1476e89da641d483d0fa602187b29a9021
Helmi17 <alauddin3helmi@gmail.com>
2024-07-23 15:57:23 +0700

    last

--- purchase_custome/__manifest__.py
+++ purchase_custome/__manifest__.py
@@ -2,13 +2,13 @@
     'name': 'Purchase Custom',
     'category': 'Accounting',
     'description': 'odoo Purchase Custom',
-    'depends': ['account', 'purchase', 'sale', 'base','sale_custome', 'hr'],
+    'depends': ['account', 'purchase', 'sale', 'base','sale_custome', 'hr','stock'],
     'data': [
         "views/purchase_views.xml",
         'views/requisition_views.xml',
         "wizard/count_quotation_views.xml",
         "views/payment_request_views.xml",
-        "views/stock_picking_views.xml",
+        # "views/stock_picking_views.xml",
         "wizard/purchase_order_wizard.xml"
     ],
 
