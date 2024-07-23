f67e9681bad9e085136b04ebe5c42255883a72f8
Helmi17 <alauddin3helmi@gmail.com>
2024-07-23 14:18:04 +0700

    komputasi

--- sale_custome/models/request_price.py
+++ sale_custome/models/request_price.py
@@ -14,6 +14,51 @@ class RequestPrice(models.Model):
     request_line_ids = fields.One2many('request.price.line', 'request_id')
     request_other_line_ids = fields.One2many('request.price.other.line', 'request_id')
     partner_id = fields.Many2one('res.partner')
+    total_price = fields.Float(compute="_total_total_price")
+    total_shipment_cost = fields.Float(compute="_total_shipment_cost")
+    total_duty = fields.Float()
+    total_tax = fields.Float()
+    total_duty_tax = fields.Float(compute="_compute_total_pajak")
+
+    @api.depends('request_line_ids.tax', 'request_line_ids.duty')
+    def _compute_total_pajak(self):
+        for line in self:
+            cost = 0
+            for lines in line.request_line_ids:
+                cost += lines.duty + lines.tax
+            line.total_duty_tax = int(cost)
+
+    @api.depends('request_line_ids.cost_price')
+    def _total_total_price(self):
+        for line in self:
+            cost = 0
+            for lines in line.request_line_ids:
+                cost += lines.cost_price * lines.quantity
+            line.total_price = cost
+
+    @api.depends('request_line_ids.shipment_cost')
+    def _total_shipment_cost(self):
+        for line in self:
+            cost = 0
+            for lines in line.request_line_ids:
+                cost += lines.shipment_cost
+            line.total_shipment_cost = cost
+
+    @api.depends('request_line_ids.duty')
+    def _total_total_duty(self):
+        for line in self:
+            cost = 0
+            for lines in line.request_line_ids:
+                cost += lines.duty
+            line.total_duty = cost
+
+    @api.depends('request_line_ids.tax')
+    def _total_total_tax(self):
+        for line in self:
+            cost = 0
+            for lines in line.request_line_ids:
+                cost += lines.tax
+            line.total_tax = cost
 
     def action_print_report(self):
         for line in self:
@@ -38,6 +83,16 @@ class RequestPrice(models.Model):
 
             return self.env.ref('sale_custome.action_report_request_price').with_context(
                 paperformat=4, landscape=False).report_action(self, data=data)
+
+    def action_other_cost(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Create Other Cost',
+            'res_model': 'price.compute',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new',
+        }
 
     def action_confirm(self):
         for line in self:
@@ -77,6 +132,9 @@ class RequestPriceLine(models.Model):
     brand = fields.Char()
     cost_price = fields.Float()
     weight = fields.Float()
+    shipment_cost = fields.Float()
+    duty = fields.Float()
+    tax = fields.Float()
     other_cost = fields.Many2many('other.price.line')
     other_price = fields.Float()
     show_other_price = fields.Boolean(compute="_compute_show")
@@ -87,9 +145,31 @@ class RequestPriceLine(models.Model):
     quantity = fields.Float(string='Quantity', digits='Product Unit of Measure', required=True, default=1.0)
     subtotal = fields.Float(
         string='Subtotal',
-        compute='_compute_totals', store=True,
+        compute='_compute_totals',
         # currency_field=None,
     )
+    percentage = fields.Float(compute="_compute_percentage")
+    total_price = fields.Float(compute="_compute_total_price")
+    final_cost = fields.Float(compute="_compute_final")
+
+    @api.depends('total_price', 'shipment_cost')
+    def _compute_final(self):
+        for line in self:
+            cost = line.total_price + (line.shipment_cost + line.duty + line.tax)
+            cost = cost / line.quantity
+            line.final_cost = round(cost)
+
+    @api.depends('cost_price')
+    def _compute_total_price(self):
+        for line in self:
+            line.total_price = line.cost_price * line.quantity
+
+    @api.depends('cost_price')
+    def _compute_percentage(self):
+        for line in self:
+            total = line.request_id.total_price
+            cost = line.total_price / total
+            line.percentage = round(cost * 100)
 
     @api.depends('other_cost')
     def _compute_show(self):
@@ -98,16 +178,16 @@ class RequestPriceLine(models.Model):
             if line.other_cost:
                 line.show_other_price = True
 
-    @api.depends('quantity', 'cost_price', 'other_price')
+    @api.depends('quantity', 'final_cost', 'other_price')
     def _compute_totals(self):
         for line in self:
             # if line.display_type != 'product':
             #     line.price_total = line.price_subtotal = False
             # # Compute 'price_subtotal'.
             # line_discount_price_unit = line.price_unit * (1 - (line.discount / 100.0))
-            subtotal = line.quantity * line.cost_price
+            subtotal = line.quantity * line.final_cost
             if line.other_price:
-                line.subtotal = subtotal + line.other_price
+                subtotal = subtotal + line.other_price
             else:
                 line.subtotal = subtotal
 
@@ -126,6 +206,3 @@ class OtherPriceLine(models.Model):
 
     name = fields.Char()
     description = fields.Text()
-
-
-
