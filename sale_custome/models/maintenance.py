e1b8fae7f257f770a73c8d74eb8fc9732f0cf29c
rusnia808 <niadyah1@gmail.com>
2025-01-20 07:43:03 +0000

    update spelling on management report

--- sale_custome/models/maintenance.py
+++ sale_custome/models/maintenance.py
@@ -12,7 +12,7 @@ class MaintenanceReport(models.Model):
     user_id = fields.Many2one('res.users', default=lambda self: self.env.uid)
     date = fields.Date()
     type = fields.Selection([
-        ('geps', 'GEPS'),
+        ('gps', 'GPS'),
         ('cctv', 'CCTV'),
         ('maintenance', 'Maintenance'),
     ])
