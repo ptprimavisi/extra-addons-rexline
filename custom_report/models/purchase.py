b85486f199af18a1d804762ebe86a0c5b83cb5db
Helmi17 <alauddin3helmi@gmail.com>
2025-04-08 15:51:33 +0700

    PO REPORT

--- custom_report/models/purchase.py
+++ custom_report/models/purchase.py
@@ -39,72 +39,73 @@ class PurchaseOrderInh(models.Model):
             # else:
             #     tags_info = so_name
 
-            # tax_data = defaultdict(lambda: {'percentage': 0, 'tax_amount': 0})
+            tax_data = defaultdict(lambda: {'percentage': 0, 'tax_amount': 0})
             #
-            # order_lines = []
-            # quotation_lines = []
-            # subtotal = 0
-            #
-            # for order_line in rec.order_line:
-            #     if order_line.product_id and order_line.product_uom_qty > 0:
-            #         for tax in order_line.tax_id:
-            #             # Hitung total pajak untuk setiap baris
-            #             tax_result = tax.compute_all(
-            #                 order_line.price_unit,
-            #                 rec.currency_id,
-            #                 order_line.product_uom_qty,
-            #                 product=order_line.product_id,
-            #                 partner=rec.partner_id
-            #             )
-            #             # Proses hasil pajak
-            #             for tax_detail in tax_result['taxes']:
-            #                 tax_name = tax.name
-            #                 tax_percentage = tax.amount  # Persentase pajak
-            #
-            #                 # Kelompokkan berdasarkan nama pajak dan persentase
-            #                 key = (tax_name, tax_percentage)
-            #                 tax_data[key]['percentage'] = tax_percentage
-            #                 tax_data[key]['tax_amount'] += tax_detail['amount']
-            #
-            #         tax_name = ""
-            #         if order_line.tax_id:
-            #             tax_name = ", ".join(tax.name for tax in order_line.tax_id)
-            #         else:
-            #             tax_name = '-'
-            #         order_lines.append([str(order_line.product_uom_qty) + ' ' + str(order_line.product_uom.name),
-            #                             order_line.product_id.product_tmpl_id.name, order_line.name,
-            #                             f"{int(order_line.price_unit):,}", "0", tax_name,
-            #                             f"{int(order_line.price_subtotal):,}", f"{int(order_line.tax_base):,}"])
-            #         quotation_lines.append(
-            #             [order_line.name, str(order_line.product_uom_qty) + ' ' + str(order_line.product_uom.name), '0',
-            #              f"{int(order_line.price_unit):,}", f"{int(order_line.price_subtotal):,}",
-            #              f"{int(order_line.tax_base):,}"])
-            #         subtotal += order_line.price_subtotal
-            #
-            # # Format hasil
-            # taxes = [{'name': key[0], 'percentage': key[1], 'amount': f"{int(values['tax_amount']):,}"}
-            #          for key, values in tax_data.items()]
-            #
-            # total_tax_base = f"{int(rec.amount_tax_base):,}"
-            #
-            # subtotal = f"{int(subtotal):,}"
-            # balance_due = f"{int(rec.amount_total):,}"
-            # balance_due_in_word = num2words(rec.amount_total, lang='en').upper()
-            #
-            # company_logo = self.env.company.logo
-            # logo = (
-            #     f"data:image/png;base64,{self.env.company.sale_logo.decode('utf-8')}"
-            #     if self.env.company.sale_logo
-            #     else None
-            # )
-            #
-            # # Get T&C
-            # so_tnc = rec.note or ''
+            order_lines = []
+            quotation_lines = []
+            subtotal = 0
+
+            for order_line in rec.order_line:
+                if order_line.product_id and order_line.product_uom_qty > 0:
+                    for tax in order_line.tax_id:
+                        # Hitung total pajak untuk setiap baris
+                        tax_result = tax.compute_all(
+                            order_line.price_unit,
+                            rec.currency_id,
+                            order_line.product_uom_qty,
+                            product=order_line.product_id,
+                            partner=rec.partner_id
+                        )
+                        # Proses hasil pajak
+                        for tax_detail in tax_result['taxes']:
+                            tax_name = tax.name
+                            tax_percentage = tax.amount  # Persentase pajak
+
+                            # Kelompokkan berdasarkan nama pajak dan persentase
+                            key = (tax_name, tax_percentage)
+                            tax_data[key]['percentage'] = tax_percentage
+                            tax_data[key]['tax_amount'] += tax_detail['amount']
+
+                    tax_name = ""
+                    if order_line.tax_id:
+                        tax_name = ", ".join(tax.name for tax in order_line.tax_id)
+                    else:
+                        tax_name = '-'
+                    order_lines.append([str(order_line.product_uom_qty) + ' ' + str(order_line.product_uom.name),
+                                        order_line.product_id.product_tmpl_id.name, order_line.name,
+                                        f"{int(order_line.price_unit):,}", "0", tax_name,
+                                        f"{int(order_line.price_subtotal):,}", f"{int(order_line.tax_base):,}"])
+                    quotation_lines.append(
+                        [order_line.name, str(order_line.product_uom_qty) + ' ' + str(order_line.product_uom.name), '0',
+                         f"{int(order_line.price_unit):,}", f"{int(order_line.price_subtotal):,}",
+                         f"{int(order_line.tax_base):,}"])
+                    subtotal += order_line.price_subtotal
+
+            # Format hasil
+            taxes = [{'name': key[0], 'percentage': key[1], 'amount': f"{int(values['tax_amount']):,}"}
+                     for key, values in tax_data.items()]
+
+            total_tax_base = f"{int(rec.amount_tax_base):,}"
+
+            subtotal = f"{int(subtotal):,}"
+            balance_due = f"{int(rec.amount_total):,}"
+            balance_due_in_word = num2words(rec.amount_total, lang='en').upper()
+
+            company_logo = self.env.company.logo
+            logo = (
+                f"data:image/png;base64,{self.env.company.sale_logo.decode('utf-8')}"
+                if self.env.company.sale_logo
+                else None
+            )
+            raise UserError(order_lines)
+
+            # Get T&C
+            po_tnc = rec.notes or ''
             # bank_name = rec.bank_name or ''
             # bank_branch = rec.bank_branch or ''
             # bank_number = rec.bank_number or ''
             # bank_account_name = rec.bank_account_name or ''
-            #
+
             # # Get Manager Signature
             # signature = self.env['sale.manager.signature'].search([], limit=1)
             # signature_name = signature.employee_id.name
@@ -131,12 +132,12 @@ class PurchaseOrderInh(models.Model):
                 'po_date_due': po_date_due,
                 # 'so_term': str(payment_term),
                 # 'customer_ref': str(customer_ref),
-                # 'so_tnc': so_tnc,
+                'po_tnc': po_tnc,
                 # 'balance_due': balance_due,
                 # 'subtotal': subtotal,
                 # 'tags_info': tags_info,
                 # 'taxes': taxes,
-                # 'order_lines': order_lines,
+                'order_lines': order_lines,
                 # 'quotation_lines': quotation_lines,
                 # 'company_logo': company_logo,
                 # 'signature_name': signature_name,
