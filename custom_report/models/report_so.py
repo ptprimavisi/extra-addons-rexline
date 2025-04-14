f217ba5e600863ea7211d9743bccf5771105d20a
Helmi17 <alauddin3helmi@gmail.com>
2025-04-14 13:59:10 +0700

    SO

--- custom_report/models/report_so.py
+++ custom_report/models/report_so.py
@@ -225,7 +225,7 @@ class InheritSaleOrder(models.Model):
         for recs in self:
             report_data=self.get_data(recs)
             return self.env.ref('custom_report.action_report_so').with_context(
-                paperformat=4, landscape=False).report_action(self, data=recs)
+                paperformat=4, landscape=False).report_action(self)
 
     def action_print_custom_quotation(self):
         for recs in self:
