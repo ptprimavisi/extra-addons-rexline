23fd3147810684c94f7a3feb6804fdb8cac34efd
Helmi17 <alauddin3helmi@gmail.com>
2025-04-14 11:04:12 +0700

    SO

--- custom_report/models/report_so.py
+++ custom_report/models/report_so.py
@@ -69,6 +69,10 @@ class InheritSaleOrder(models.Model):
     expense_count=fields.Integer(readonly=True)
     customer_ref = fields.Char()
 
+    def get_report_filename(self, report):
+        self.ensure_one()
+        return self.name  # langsung ambil dari field model
+
     def get_data(self,recs):
         for rec in recs:
             company = self.env.user.company_id
