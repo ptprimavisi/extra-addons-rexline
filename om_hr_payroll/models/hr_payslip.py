3c7b6309ace81e1a8b102a01956f0d3cfd89d6c2
Helmi17 <alauddin3helmi@gmail.com>
2025-05-20 12:17:03 +0700

    HR

--- om_hr_payroll/models/hr_payslip.py
+++ om_hr_payroll/models/hr_payslip.py
@@ -740,7 +740,7 @@ class HrPayslip(models.Model):
                         gaji_net.amount = gaji_net.amount - abs_ded_amount
                         print(f'{int(abs_ded_amount)} -- {jumlah_tdk_absence}')
                         test = f'amount = {int(abs_ded_amount)} -- jumlah tidak absen = {jumlah_tdk_absence}'
-                        raise UserError()
+                        raise UserError(test)
 
             pph = self.env['hr.payslip.line'].search(
                 [('slip_id', '=', int(payslip.id)), ('code', 'in', ['PPH21'])])
